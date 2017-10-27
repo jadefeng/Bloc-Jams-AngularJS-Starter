dac627413b7eea8c9964ee15767e6b9fe9422051
jadefeng <jade.jiefeng@gmail.com>
2017-10-26 21:54:49 -0700

    Adding thumbScope

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -17,19 +17,19 @@
 	         scope: { },
 	         link: function(scope,element,attributes) {
 	         	// directive logic
-	             scope.value = 0;
-	             scope.max = 100;
+	             scope.value = 0; // Holds the value of the seek bar, such as the currently playing song time or the current volume. Default value is 0.
+	             scope.max = 100; // Holds the maximum value of the song and volume seek bars. Default value is 100.
 	 
 	 			 var seekBar = $(element);
 
-	             var percentString = function () {
+	             var percentString = function () { // A function that calculates a percent based on the value and maximum value of a seek bar.
 	                 var value = scope.value;
 	                 var max = scope.max;
 	                 var percent = value / max * 100;
 	                 return percent + "%";
 	             };
 	 
-	             scope.fillStyle = function() {
+	             scope.fillStyle = function() { // Returns the width of the seek bar fill element based on the calculated percent.
 	                 return {width: percentString()};
 	             };
 
@@ -37,6 +37,10 @@
 		             var percent = calculatePercent(seekBar, event);
 		             scope.value = percent * scope.max;
 		         };
+
+		         scope.thumbStyle = function() { // updates the position of the seek bar thumb
+					return {left: percentString()};	
+		         }
 
 
 		          scope.trackThumb = function() {
